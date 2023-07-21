cb3b9bce123a3521509a274b4af66c837f39286e
sun-a-i <taeyang35@gmail.com>
2023-07-21 16:18:57 +0900

    modi

--- 03STS_Workspace/WebtoonPage/src/main/java/com/kopo/repository/WebtoonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kopo.repository;
-
-import java.util.List;
-
-import com.kopo.domain.Webtoon;
-
-public interface WebtoonRepository {
-	List<Webtoon> getAllWebtoonList();
-}
--- 03STS_Workspace/WebtoonPage/src/main/java/com/kopo/service/WebtoonService.java
+++
@@ -1,9 +0,0 @@
-package com.kopo.service;
-
-import java.util.List;
-
-import com.kopo.domain.Webtoon;
-
-public interface WebtoonService {
-	List<Webtoon> getAllWebtoonList();
-}
